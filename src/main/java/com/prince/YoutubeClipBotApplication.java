f82cef6875461955d2cc8e3bcfb500c2e94c3da3
numbnumb159-boop <numbnumb159@gmail.com>
2026-02-13 11:51:26 +0530

    Create YoutubeClipBotApplication.java

---
+++ src/main/java/com/prince/YoutubeClipBotApplication.java
@@ -0,0 +1,12 @@
+package com.prince;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeClipBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoutubeClipBotApplication.class, args);
+    }
+}
